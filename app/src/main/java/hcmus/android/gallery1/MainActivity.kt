c8137616a008c90fd1bdaa45ab8899a8a424ae88
84436 <thien.bui.84436@gmail.com>
2021-03-08 09:21:01 +0700

    Splash screen + Icon (day/night); Manifest: disable app backup

--- app/src/main/java/hcmus/android/gallery1/MainActivity.kt
+++ app/src/main/java/hcmus/android/gallery1/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Reset: splash screen "theme" -> default theme
+        setTheme(R.style.Theme_GalleryOne)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
