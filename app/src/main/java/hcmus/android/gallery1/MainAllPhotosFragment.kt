3b54423ebfe9cc1e1ee84ab54f6d1a53fa3a3615
84436 <thien.bui.84436@gmail.com>
2021-04-20 19:51:38 +0700

    MainAllPhotosFragment: init properly (but not really)

--- app/src/main/java/hcmus/android/gallery1/MainAllPhotosFragment.kt
+++ app/src/main/java/hcmus/android/gallery1/MainAllPhotosFragment.kt
@@ -1,6 +1,7 @@
 package hcmus.android.gallery1
 
 import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.provider.MediaStore
@@ -12,54 +13,41 @@ import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
-// private const val ARG_PARAM1 = "param1"
+import hcmus.android.gallery1.helpers.PreferenceFacility
 
 class MainAllPhotosFragment : Fragment() {
     private lateinit var blogAdapter: RecycleAdapter
-    // private var param1: String? = null
+    private lateinit var layoutManager: RecyclerView.LayoutManager
+
+    // PASS THIS THING; DO NOT RECREATE PER CLASS
+    private lateinit var prefs : PreferenceFacility
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        addDataSet();
-        arguments?.let {
-            // param1 = it.getString(ARG_PARAM1)
-        }
+        prefs = PreferenceFacility(requireContext().getSharedPreferences("MainActivity", Context.MODE_PRIVATE))
+        blogAdapter = RecycleAdapter()
+        addDataSet()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main_all_photos, container, false)
+        // Inflate the root view
+        val fragmentView = inflater.inflate(R.layout.fragment_main_all_photos, container, false)
+
+        // Create layout manager (using a dirty refresh)
+
+        // Init that RecyclerView
+        fragmentView.findViewById<RecyclerView>(R.id.recycler_view).apply {
+            layoutManager = GridLayoutManager(requireContext(), 4)
+            adapter = blogAdapter
+        }
+        return fragmentView
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance() =  MainAllPhotosFragment().apply {
-            arguments = Bundle().apply {
-                // putString(ARG_PARAM1, param1)
-            }
-        }
-    }
     private fun addDataSet(){
         val data = DataSource.createDataSet()
         blogAdapter.submitList(data)
     }
-
-    private fun initRecycleView(){
-        // Initialize the adapter
-        blogAdapter = RecycleAdapter()
-
-        // Find the RecyclerView on main activity, then init (2)
-        val myRecyclerView = layoutInflater.inflate(R.layout.fragment_main_all_photos,null)?:return
-        myRecyclerView.findViewById<RecyclerView>(R.id.recycler_view)
-        myRecyclerView.apply {
-            // layoutManager = LinearLayoutManager(this@MainActivity)
-            layoutManager = GridLayoutManager(this, 4)
-            adapter = blogAdapter
-        }
-    }
-
-
 }
